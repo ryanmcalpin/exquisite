6cf583140f5449a3f3696ef5c92ff8aff3f119f0
Ryan McAlpin <rhmcalpin@gmail.com>
2017-06-20 15:19:43 -0700

    remove game from collaboratorInvites table in firebase once invite is accepted

--- app/src/main/java/com/epicodus/exquisite/adapters/FirebaseInviteViewHolder.java
+++ app/src/main/java/com/epicodus/exquisite/adapters/FirebaseInviteViewHolder.java
@@ -1,6 +1,8 @@
 package com.epicodus.exquisite.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +12,15 @@ import android.widget.Toast;
 import com.epicodus.exquisite.Constants;
 import com.epicodus.exquisite.R;
 import com.epicodus.exquisite.models.Game;
+import com.epicodus.exquisite.ui.GameActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class FirebaseInviteViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+public class FirebaseInviteViewHolder extends RecyclerView.ViewHolder{
     View mView;
     Context mContext;
     FirebaseUser mUser;
@@ -24,7 +29,6 @@ public class FirebaseInviteViewHolder extends RecyclerView.ViewHolder implements
         super(itemView);
         mView = itemView;
         mContext = itemView.getContext();
-        itemView.setOnClickListener(this);
 
         mUser = FirebaseAuth.getInstance().getCurrentUser();
     }
@@ -48,15 +52,22 @@ public class FirebaseInviteViewHolder extends RecyclerView.ViewHolder implements
             public void onClick(View v) {
                 game.setCollaboratorName(mUser.getDisplayName());
                 game.setCollaboratorUid(mUser.getUid());
-                DatabaseReference gameRef = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_GAMES).child(game.getOwnerUid()).child(game.getFirebaseKey());
-                gameRef.setValue(game);
-                Toast.makeText(mContext, "YAAY", Toast.LENGTH_SHORT).show();
+                DatabaseReference ownerGameRef = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_GAMES).child(game.getOwnerUid()).child(game.getFirebaseKey());
+                ownerGameRef.setValue(game).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            DatabaseReference collabGameRef = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_GAMES).child(game.getCollaboratorUid()).child(game.getFirebaseKey());
+                            collabGameRef.setValue(game);
+                            DatabaseReference inviteRef = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_COLLABORATOR_INVITES).child(game.getCollaboratorUid()).child(game.getFirebaseKey());
+                            inviteRef.removeValue();
+                            Intent intent = new Intent(mContext, GameActivity.class);
+                            mContext.startActivity(intent);
+                        }
+                    }
+                });
+
             }
         });
-    }
-
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(mContext, "", Toast.LENGTH_SHORT).show();
     }
 }
