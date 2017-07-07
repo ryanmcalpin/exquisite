36ccc3aad72e9989bc0c3adadc92bdd223717b2e
Ryan McAlpin <rhmcalpin@gmail.com>
2017-07-06 18:19:47 -0700

    refactor Constants to reflect new database structure

--- app/src/main/java/com/epicodus/exquisite/Constants.java
+++ app/src/main/java/com/epicodus/exquisite/Constants.java
@@ -3,6 +3,7 @@ package com.epicodus.exquisite;
 public class Constants {
     public static final String FIREBASE_NICKNAMES = "nicknames";
     public static final String FIREBASE_GAMES = "games";
-    public static final String FIREBASE_COLLABORATOR_INVITES = "collaboratorInvites";
+    public static final String FIREBASE_USER_GAMES = "userGames";
+    public static final String FIREBASE_INVITES = "invites";
     public static final String PREFS_COLLABS_KEY = "collaborators";
 }
